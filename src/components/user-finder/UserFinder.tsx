f7f9a9c1404fc151c17a30a207be3406f09f5cbf
Miran Korkmaz <mirankorkmaz@outlook.com>
2022-07-27 19:09:09 +0200

    Add mock data

--- src/components/user-finder/UserFinder.tsx
+++ src/components/user-finder/UserFinder.tsx
@@ -1,17 +1,49 @@
-import "./UserFinder.css"
+import { FC, useState } from "react";
+import "./UserFinder.css";
 
-import { FC } from "react";
 
 export const UserFinder: FC = () => {
+    const users = [
+        {
+            name: "Ronni",
+            age: 23,
+            designation: "Stockbroker"
+        },
+        {
+            name: "Miran",
+            age: 25,
+            designation: "Software Engineer"
+        },
+        {
+            name: "Matin",
+            age: 24,
+            designation: "Law Student"
+        },
+        {
+            name: "Ala",
+            age: 1.5,
+            designation: "A Cat"
+        },
+    ];
+
+    const [text, setText] = useState("");
     return (
         <div>
             <div className="title">
                 <h1>User Finder</h1>
             </div>
             <div className="input-wrapper">
-                <input type="text" />
+                <input 
+                    type="text"
+                    placeholder="Search User"
+                    value={text}
+                    onChange={e => setText(e.target.value)}
+                    />
                 <button>Search</button>
+            </div>
+            <div className="body">
+
             </div>
         </div>
     )
-}
+};
